774c55095e8f364363158be8d9a6d83444a6506f
William Liu <superliuxz@gmail.com>
2018-05-28 14:58:25 -0700

    add shadow

--- Lego/make_synthetic_data.py
+++ Lego/make_synthetic_data.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 from matplotlib.transforms import Bbox
 import numpy as np
 
@@ -6,6 +7,7 @@ import csv
 
 
 color = {0: 'royalblue', 1: 'crimson'}
+#color = {0: 'crimson', 1: 'crimson'}
 
 
 def pretty_print(board):
@@ -16,23 +18,26 @@ def gen_board(filename):
     base = np.zeros((16, 32))
 
     fig, ax = plt.subplots(figsize=(4, 3))
-    fig.patch.set_facecolor(np.random.rand(4))
+    #fig.patch.set_facecolor(np.random.rand(4))
 
     plt.imshow(base, cmap='Greys')
 
-    r = plt.Rectangle((29.5, 0.5), 1, 1, fill=True, color=color[0])
+    r = plt.Rectangle((29.5, 0.5), 1, 1, fill=True, color=color[0],
+        path_effects=[path_effects.withSimplePatchShadow(offset=(np.random.randint(-5, 6), np.random.randint(-5, 6)))])
 
     ax.add_artist(r)
     base[1][30] = 200
 
     for i in range(np.random.randint(1, 11)):
-        l = np.random.randint(1, 9)
+        l = np.random.randint(1, 7)
         x = np.random.randint(1, 31-l)-0.5
         y = np.random.randint(1, 15)-0.5
         c = np.random.randint(0, 2)
-        r = plt.Rectangle((x, y), l, 1, fill=True, color=color[c])
+        offset = (np.random.randint(-5, 6), np.random.randint(-5, 6))
+        r = plt.Rectangle((x, y), l, 1, fill=True, color=color[c], path_effects=[path_effects.withSimplePatchShadow(offset=offset)])
         for j in range(l):
             if c == 0:
+                #base[int(y+0.5)][int(x+0.5+j)] = 100
                 base[int(y+0.5)][int(x+0.5+j)] = 200
             elif c == 1:
                 base[int(y+0.5)][int(x+0.5+j)] = 100
@@ -64,8 +69,10 @@ if __name__ == '__main__':
     res = []
     
     for i in range(5000):
+        print(f'working on {i}...')
         r = gen_board(str(i+1).zfill(5))
         res.append(r)
     with open('output.txt', 'w') as fout:
         writer = csv.writer(fout, delimiter=',')
         writer.writerows(res)
+    print('done')
